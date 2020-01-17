f7c6d26590bb37d787acca48f73197940d33c6f0
caifan <caifan@bigo.sg>
2020-01-17 10:58:12 +0800

    1

--- src/main.py
+++ src/main.py
@@ -18,6 +18,22 @@ class Solution(object):
         """
         return n % 4 != 0
 
+    def largestPerimeter(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        if len(A) < 3:
+            return 0
+        A.sort()
+        index = len(A) - 1
+        while index >= 2:
+            if (A[index - 1] + A[index - 2] > A[index]):
+                return A[index - 1] + A[index - 2] + A[index]
+            index -= 1
+        return 0
+
+
 if __name__ == "__main__":
     s1 = Solution()
     print s1.detectCapitalUse("mL")
